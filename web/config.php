749778848facf4c64018cd616003480dafd6ef83
i0day <48622971+i0day@users.noreply.github.com>
2019-03-16 10:24:08 -0400

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyAurg5k-lp9qQxA_IpC5x48YZ3-cslEQ5Q');
-define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
-define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('GJ_CODE', 'US');
+define('SITE_NAME', '耗子视频');
+define('TITLENAME', '耗子视频');
+define('EN2DEKEY', 'woaihaozi');
+define('EMAIL', 'test@gmail.com');
 ?>
